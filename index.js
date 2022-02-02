233e25e5e3fc1876029afe533049fd102cf8bf6f
christian.backstrom <christian.berggren.backstrom@gmail.com>
2022-02-02 09:12:19 +0100

    removed logs

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ client.on('messageCreate', (message) => {
     if (!message.content.startsWith(prefix) || message.author.bot) return
 
 	const args = message.content.slice(prefix.length).trim().split(' ')
-    console.log(args);
 	const command = args.shift().toLowerCase()
 
     if (command === 'playbruh'){
